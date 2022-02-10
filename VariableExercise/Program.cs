eba5aab3d44e571b729f9ae7b11cac8c333fa81c
Devan <deshlock010101@hotmai.com>
2022-02-10 14:38:02 -0600

    Declared, initialized, and printed on console.

--- VariableExercise/Program.cs
+++ VariableExercise/Program.cs
@@ -6,6 +6,33 @@ namespace VariableExercise
     {
         static void Main(string[] args)
         {
+            string user_name;
+            int age;
+            char first_initial;
+            bool true_false;
+            double a;
+            decimal b;
+
+            user_name = "Devan";
+            age = 35;
+            first_initial = 'D';
+            true_false = true;
+            a = 64;
+            b = 13.2m;
+
+            Console.WriteLine(user_name);
+            Console.WriteLine(age);
+            Console.WriteLine(first_initial);
+            Console.WriteLine(true_false);
+            Console.WriteLine(a);
+            Console.WriteLine(b);
+              
+
+            
+            
+            
+            
+            
             //Console.WriteLine("Hello World!");
         }
     }
